9fb328f2398d066ac90c27291a914d9f39a402ae
Vitalii Savchuk <esvit666@gmail.com>
2013-12-16 08:39:05 +0200

    build

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ requirejs.config({
         'jquery-ui': '../bower_components/jquery-ui/ui',
 
         //jquery-galleria
-        'jquery-galleria': '../bazalt/bower_components/jquery-galleria/src/galleria',
+        'jquery-galleria': '../bower_components/jquery-galleria/src/galleria',
 
         'bootstrap': '../bower_components/bootstrap/js',
 
